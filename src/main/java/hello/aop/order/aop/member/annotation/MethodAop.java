216863218e818c7cce4cabcfe1698e29ed5cf30c
nickhealthy <alskadmlcraz1@gmail.com>
2024-11-16 15:42:53 +0900

    comment: 11. 스프링 AOP 포인트컷 - 예제만들기

--- src/main/java/hello/aop/order/aop/member/annotation/MethodAop.java
+++ src/main/java/hello/aop/order/aop/member/annotation/MethodAop.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 강의: 11. 예제 만들기
+ * 실습 목표: 커스텀한 애노테이션 기반으로 포인트컷을 적용해본다.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MethodAop {
