2c9865deaa10d7382d71abfdadd6bd0e0a9a7426
nickhealthy <alskadmlcraz1@gmail.com>
2024-11-17 21:36:48 +0900

    13. 스프링 AOP 실무 주의사항 - 프록시 기술과 한계 - 스프링의 해결책

--- src/test/java/hello/aop/proxyvs/ProxyDITest.java
+++ src/test/java/hello/aop/proxyvs/ProxyDITest.java
@@ -10,13 +10,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Slf4j
+// //@SpringBootTest(properties = "spring.aop.proxy-target-class=true") 설정 없이도 스프링 부트 2.0 이상부터 기본적으로 CGLIB 프록시를 사용하게 된다.
 @Import(ProxyDIAspect.class)
 /*
 * Bean named 'memberServiceImpl' is expected to be of type 'hello.aop.member.MemberServiceImpl' but was actually of type 'jdk.proxy3.$Proxy56'
 * JDK 동적 프록시, DI 예외 발생
 * */
 //@SpringBootTest(properties = "spring.aop.proxy-target-class=false")
-@SpringBootTest(properties = "spring.aop.proxy-target-class=true")
+//@SpringBootTest(properties = "spring.aop.proxy-target-class=true")
+@SpringBootTest
 public class ProxyDITest {
 
     @Autowired
