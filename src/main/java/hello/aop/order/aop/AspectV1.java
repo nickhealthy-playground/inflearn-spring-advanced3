1224eadd36374c23e64d2d90ecc9221205f1bf44
nickhealthy <alskadmlcraz1@gmail.com>
2025-03-15 18:29:31 +0900

    9. 스프링 AOP 구현1 - 시작

---
+++ src/main/java/hello/aop/order/aop/AspectV1.java
@@ -0,0 +1,26 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * @Aspect 사용
+ */
+@Slf4j
+@Aspect // 해당 애노테이션은 단순히 애스펙트 표식 -> 컴포넌트 스캔 대상이 되는 것은 아님
+public class AspectV1 {
+
+    /**
+     * 어드바이저: 어드바이스 + 포인트컷
+     * 어드바이스: doLog 메서드
+     * 포인트컷: execution(* hello.aop.order..*(..))
+     */
+    // hello.aop.order 패키지와 그 하위 패키지(..)
+    @Around("execution(* hello.aop.order..*(..))")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature()); // join point 시그니처
+        return joinPoint.proceed();
+    }
+}
--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -2,16 +2,19 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
+@Import(AspectV1.class) // 주로 설정 파일을 추가할 때 사용, 해당 애노테이션을 이용해 스프링 빈으로 등록 가능
 @SpringBootTest
 public class AopTest {
 
